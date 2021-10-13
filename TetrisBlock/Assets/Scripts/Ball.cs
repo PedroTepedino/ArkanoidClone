11cd491bd40ae83c58af35d231c7d4ca1f96c2e5
tepe <pedrotepedinossauro@gmail.com>
2021-10-12 22:54:37 -0300

    Add Ball

---
+++ TetrisBlock/Assets/Scripts/Ball.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Vector2 direction = new Vector2(1,1);
+    [SerializeField] private float speed = 5;
+
+    private Vector2 velocity = Vector2.zero;
+    
+    private Rigidbody2D body;
+
+    private bool isLocked = true;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        velocity = direction.normalized * speed;
+        isLocked = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            isLocked = false;
+        
+        if (isLocked) return;
+        
+        body.velocity = velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var contactAngle = Vector2.Angle(Vector2.right, other.contacts[0].point - (Vector2)this.transform.position);
+
+        if (contactAngle >= 45 && contactAngle <= 135)
+        {
+            velocity.y = -velocity.y;
+        }
+
+        if (!(contactAngle > 45 && contactAngle < 135))
+        {
+            velocity.x = -velocity.x;
+        }
+    }
+    
+  
+}
